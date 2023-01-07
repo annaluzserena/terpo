02fc711993d089efa7cb34ee6ed86814b83b30ad
Anna <annaluzserena@protonmail.com>
2023-01-07 14:24:10 -0300

    Estructura desktop

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     let serviciosLi = document.querySelector(".servicios");
-    let dropdown = document.querySelector(".d-servicios");
+    let dropdown = document.querySelectorAll(".d-servicios");
     let listaNav = document.querySelector(".lista-nav");
     let dropMenu = document.querySelector(".menu-drop");
     let logoHeader = document.querySelector(".logo-header");
@@ -8,9 +8,18 @@ window.addEventListener("load", function() {
     let footer = document.querySelector(".hide-container")
     let fixedHeader = document.querySelector(".cont-header-2");
 
+    if (window.innerWidth >= "620") {
+        logoHeader.src = "/img/TERPO-BLANCO.svg";
+        dropdown[0].classList.add("hide-mobile");
+        dropdown[1].classList.remove("hide-desktop");
+    } else if (window.innerWidth < "620") {
+        dropdown[0].classList.remove("hide-mobile");
+        dropdown[1].classList.add("hide-desktop");
+    }
+
     let lastScroll = 0;
 
-    window.addEventListener("scroll", function() {
+    window.addEventListener("scroll", function () {
         const currentScroll = window.pageYOffset;
         let navPosition = fixedHeader.getBoundingClientRect();
         console.log(currentScroll);
@@ -21,12 +30,12 @@ window.addEventListener("load", function() {
             fixedHeader.classList.remove("fixed");
         };*/
 
-        if(currentScroll > lastScroll && !listaNav.classList.contains("folded")) {
+        if (window.innerWidth < "620" && currentScroll > lastScroll && !listaNav.classList.contains("folded")) {
             listaNav.classList.remove("unfolded");
             listaNav.classList.add("folded");
             logoHeader.classList.add("after");
             menuUp[1].classList.add("drop-down");
-        } else if(currentScroll < lastScroll && listaNav.classList.contains("folded")) {
+        } else if (window.innerWidth < "620" && currentScroll < lastScroll && listaNav.classList.contains("folded")) {
             listaNav.classList.remove("folded");
             listaNav.classList.add("unfolded");
             logoHeader.classList.remove("after");
@@ -44,17 +53,23 @@ window.addEventListener("load", function() {
         lastScroll = currentScroll;
     });
 
-    serviciosLi.addEventListener("click", function() {
-        dropdown.classList.toggle("folded");
-        dropdown.classList.toggle("unfolded");
+    serviciosLi.addEventListener("click", function () {
+        if (window.innerWidth < "620") {
+            dropdown[0].classList.toggle("folded");
+            dropdown[0].classList.toggle("unfolded");
+        } else if (window.innerWidth >= "620") {
+            dropdown[1].classList.toggle("folded");
+            dropdown[1].classList.toggle("unfolded");
+            serviciosLi.classList.toggle("add-border");
+        }
         serviciosLi.classList.toggle("toggle-servicios");
         menuUp[0].classList.toggle("drop-down-s");
     });
 
-    dropMenu.addEventListener("click", function() {
+    dropMenu.addEventListener("click", function () {
         listaNav.classList.toggle("folded");
         listaNav.classList.toggle("unfolded");
         logoHeader.classList.toggle("after");
         menuUp[1].classList.toggle("drop-down");
-    })
+    });
 });
